6ab715da5bfe27ad97508eb19fb837822380b536
Jeff Spooner <jdspoone@icloud.com>
2015-12-29 12:43:18 -0700

    Initial Commit

---
+++ RecipeBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipeBook
+//
+//  Created by Jeffrey Spooner on 2015-12-29.
+//  Copyright © 2015 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
