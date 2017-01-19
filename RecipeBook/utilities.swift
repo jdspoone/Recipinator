814b306cba9995f064ecc1cd329cd4232fb101d9
Jeff Spooner <jdspoone@icloud.com>
2017-01-19 15:11:02 -0700

    When creating buttons, set image instead of background image

--- RecipeBook/utilities.swift
+++ RecipeBook/utilities.swift
@@ -11,7 +11,7 @@ func roundedSquareButton(target: AnyObject?, action: Selector, controlEvents: UI
   {
     let button = UIButton(type: .Custom)
     button.addTarget(target, action: action, forControlEvents: controlEvents)
-    button.setBackgroundImage(UIImage(named: imageName), forState: .Normal)
+    button.setImage(UIImage(named: imageName), forState: .Normal)
     button.layer.cornerRadius = 5.0
     button.layer.borderWidth = 0.5
     button.layer.borderColor = UIColor.lightGrayColor().CGColor
@@ -25,7 +25,7 @@ func button(target: AnyObject?, action: Selector, controlEvents: UIControlEvents
   {
     let button = UIButton(type: .Custom)
     button.addTarget(target, action: action, forControlEvents: controlEvents)
-    button.setBackgroundImage(UIImage(named: imageName), forState: .Normal)
+    button.setImage(UIImage(named: imageName), forState: .Normal)
     button.showsTouchWhenHighlighted = true
     button.translatesAutoresizingMaskIntoConstraints = false
     return button
